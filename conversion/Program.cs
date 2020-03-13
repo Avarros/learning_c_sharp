7d0f80e2bb0fcf635275fc05760f50472be22394
Avarros <60660200+Avarros@users.noreply.github.com>
2020-03-13 16:35:08 +0100

    Add files via upload

---
+++ conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num =Convert.ToInt32("32"); /*CONVERTING STRING TO INT*/
+            Console.WriteLine(num);
+            Console.Write("Enter number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());/*Converts user input (string) to int*/
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());/*Converts user input (string) to int*/
+            Console.WriteLine(num1+num2);
+        }
+    }
+}
