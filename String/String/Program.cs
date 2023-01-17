7d02f84630e6980b58e0b5da3f4b805813891113
Avarros <60660200+Avarros@users.noreply.github.com>
2023-01-17 18:07:51 +0100

    Update Program.cs

--- String/String/Program.cs
+++ String/String/Program.cs
@@ -7,8 +7,8 @@ namespace String
     {
         static void Main(string[] args)
         {
-            string phrase = "I love to suck fat cock";
-            Console.WriteLine(phrase.IndexOf("dick"));/*shows where it starts,if it doesn't exist it outputs -1*/
+            string phrase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt";
+            Console.WriteLine(phrase.IndexOf("sit"));/*shows where it starts,if it doesn't exist it outputs -1*/
             Console.WriteLine(phrase.IndexOf('o'));/*shows where the first instance of it is,if it doesn't exist it outputs -1*/
             Console.WriteLine(phrase.Substring(8));/*grabs something from a given place right to the end*/
             Console.WriteLine(phrase.Substring(8,6));/*grabs something from a given place right to the end,second number is how many characters we want to get*/
